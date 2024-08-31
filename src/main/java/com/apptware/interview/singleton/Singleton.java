1d20f2383cc9407f3cfc7f3eb524e154552e77dd
govindkapure <57093788+govindkapure@users.noreply.github.com>
2024-09-01 00:31:15 +0530

    Update Singleton.java

--- src/main/java/com/apptware/interview/singleton/Singleton.java
+++ src/main/java/com/apptware/interview/singleton/Singleton.java
@@ -3,16 +3,22 @@ package com.apptware.interview.singleton;
 
 public class Singleton {
   private static Singleton single_instance = null;
+  private static boolean isInstanceCreated = false;
 
   public String s;
 
   private Singleton() {
-    s = "Hello I am a string part of Singleton class";
+    if (isInstanceCreated) {
+      throw new RuntimeException("Use getInstance() method to create");
+    } else {
+      isInstanceCreated = true;
+      s = "Hello I am a string part of Singleton class";
+    }
   }
 
   public static synchronized Singleton getInstance() {
     if (single_instance == null) single_instance = new Singleton();
-
+    isInstanceCreated = true;
     return single_instance;
   }
 }
