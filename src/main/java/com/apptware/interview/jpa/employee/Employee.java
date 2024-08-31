5da6359d04b8815e81d227ce5633f0552a819b47
govindkapure <57093788+govindkapure@users.noreply.github.com>
2024-09-01 00:31:46 +0530

    Update Employee.java

--- src/main/java/com/apptware/interview/jpa/employee/Employee.java
+++ src/main/java/com/apptware/interview/jpa/employee/Employee.java
@@ -2,6 +2,7 @@ package com.apptware.interview.jpa.employee;
 
 import jakarta.persistence.Entity;
 import java.util.UUID;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
